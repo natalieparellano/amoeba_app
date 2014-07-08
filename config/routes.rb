a51698e7f1c59b0f242b4625a8ff4f7614f3d14d
natalieparellano <natalie.p.arellano@gmail.com>
2014-07-08 17:01:32 -0400

    Create amoeba table, use rails g scaffold act

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :acts
+
   resources :amoebas
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/migrate/20140708205538_create_amoebas.rb
+++ db/migrate/20140708205538_create_amoebas.rb
@@ -1,6 +1,9 @@
 class CreateAmoebas < ActiveRecord::Migration
   def change
     create_table :amoebas do |t|
+      t.string :name
+      t.string :talent
+      t.integer :generation
 
       t.timestamps
     end
